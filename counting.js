781f78016b3bab0a1fe368c78b2fd677dab1ebea
Rachael Topp <topp@ualberta.ca>
2017-10-04 16:23:21 +0000

    final result

--- counting.js
+++ counting.js
@@ -1,3 +1,14 @@
-function countLetters() {
-
+function countLetters(string) {
+  var letterCount = {};
+  string = string.replace(/ /g ,"");
+  for(var i = 0; i < string.length; i ++) {
+    var letter = string[i].toLowerCase();
+    if (letter in letterCount) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  } return letterCount;
 }
+
+console.log(countLetters('lighthouse in the house'));
